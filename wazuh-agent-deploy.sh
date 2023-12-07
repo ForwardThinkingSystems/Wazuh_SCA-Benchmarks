6c7a3e2c43d8a0bc2b5ef9e048cfd085842e5050
malindarathnayake-fts <139892485+malindarathnayake-fts@users.noreply.github.com>
2023-12-07 17:57:01 -0500

    Update wazuh-agent-deploy.sh

--- wazuh-agent-deploy.sh
+++ wazuh-agent-deploy.sh
@@ -22,10 +22,10 @@ check_error() {
 log_message "Installing Wazuh Agent"
 curl -o wazuh-agent-4.6.0-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.6.0-1.x86_64.rpm && sudo WAZUH_MANAGER='10.254.254.240' WAZUH_AGENT_GROUP='Linux_Servers' rpm -ihv wazuh-agent-4.6.0-1.x86_64.rpm
 
-log_message "Configuring Wazuh Agent"
+log_message "Configuring Wazuh Agent Service"
 sudo systemctl daemon-reload
 sudo systemctl enable wazuh-agent
-sudo systemctl start wazuh-agent --wait
+sudo systemctl start wazuh-agent
 check_error "Failed to start Wazuh Agent"
 
 log_message "Setting up CIS profile"
