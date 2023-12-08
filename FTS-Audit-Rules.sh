e830291f01011bf022690a8791fd7619b3b6b7ce
malindarathnayake-fts <139892485+malindarathnayake-fts@users.noreply.github.com>
2023-12-08 14:15:23 -0500

    Update FTS-Audit-Rules.sh

--- FTS-Audit-Rules.sh
+++ FTS-Audit-Rules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo dnf install audit -y
-sudo cp /usr/share/audit/sample-rules/30-stig.rules /home/lxadmin/30-stig.rules
+#sudo cp /usr/share/audit/sample-rules/30-stig.rules /home/lxadmin/30-stig.rules
 sudo curl https://raw.githubusercontent.com/ForwardThinkingSystems/Wazuh_SCA-Benchmarks/main/fts-audit.rules -o /etc/audit/rules.d/fts-audit.rules
 sudo service auditd reload
