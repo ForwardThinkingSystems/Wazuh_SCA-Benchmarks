0d5de7f65e84603d7ef1d11731e3af6807016593
malindarathnayake-fts <139892485+malindarathnayake-fts@users.noreply.github.com>
2024-02-28 15:42:35 -0500

    Update Nginx_FIM-deploy_config.sh

--- Nginx_FIM-deploy_config.sh
+++ Nginx_FIM-deploy_config.sh
@@ -20,7 +20,7 @@ check_error() {
 if sudo systemctl is-active --quiet wazuh-agent; then
     echo "wazuh Agent Service is active - Proceeding with the config file deployment"
     sudo rm -f "/var/ossec/etc/ossec.conf"
-    sudo cp -f "$OSSSEC_Config_nginx_Source_PATH" "$OSSSEC_Config_nginx_LOCAL_PATH"
+    sudo curl "$OSSSEC_Config_nginx_Source_PATH" "$OSSSEC_Config_nginx_LOCAL_PATH"
     check_error "Failed to copy OSSEC Config file"
     log_message "OSSEC Config file deployment - Completed"   
 else
