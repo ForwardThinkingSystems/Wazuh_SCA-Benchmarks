fd6e00aa7d7bc2c008efe795011cc059e15dd030
malindarathnayake-fts <139892485+malindarathnayake-fts@users.noreply.github.com>
2024-11-27 03:09:46 -0500

    Update wazuh-agent-deploy.sh

--- wazuh-agent-deploy.sh
+++ wazuh-agent-deploy.sh
@@ -41,14 +41,6 @@ check_error() {
     fi
 }
 
-log_message "Stopping Wazuh Service"
-sudo systemctl stop wazuh-agent
-log_message "uninstall wazuh-agent"
-sudo yum remove -y wazuh-agent
-log_message "Cleanup old files"
-sudo rm -rf /var/ossec/
-
-
 log_message "Installing Wazuh Agent"
 curl -o wazuh-agent-${WAZUH_AGENT_VERSION}.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-${WAZUH_AGENT_VERSION}.x86_64.rpm
 log_message "Configuring Wazuh Agent Service"
