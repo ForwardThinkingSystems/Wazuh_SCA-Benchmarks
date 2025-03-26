5b46c86a8885671ddfc2abccdafb6f880119eac0
malindarathnayake-fts <139892485+malindarathnayake-fts@users.noreply.github.com>
2025-03-26 17:31:13 -0400

    Update wazuh-agent-deploy.sh

--- wazuh-agent-deploy.sh
+++ wazuh-agent-deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+WAZUH_AGENT_VERSION="4.9.2-1"
 WAZUH_AGENT_GROUP='Linux_Servers'
 #CIS_PROFILE_URL='https://raw.githubusercontent.com/ForwardThinkingSystems/Wazuh_SCA-Benchmarks/main/cis_rhel8_linux.yml' --- Keeping as a reference
 CIS_PROFILE_URL=""
 CIS_PROFILE_PATH='/var/ossec/ruleset/sca/cis_rhel8_linux.yml'
 SHARED_CONFIG_PATH='/var/ossec/etc/shared/Linux_Servers/cis_rhel8_linux-FTS.yml'
+
 
 # Detect AlmaLinux version
 os_version=$(grep -oP '(?<=release )\d+' /etc/redhat-release)
@@ -39,8 +41,8 @@ check_error() {
     fi
 }
 
-sudo rpm --import https://packages.wazuh.com/key/GPG-KEY-WAZUH
-sudo cat > /etc/yum.repos.d/wazuh.repo << EOF
+rpm --import https://packages.wazuh.com/key/GPG-KEY-WAZUH
+cat > /etc/yum.repos.d/wazuh.repo << EOF
 [wazuh]
 gpgcheck=1
 gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
@@ -52,12 +54,12 @@ EOF
 
 sudo dnf update -y
 
-WAZUH_MANAGER="10.0.0.2"
-sudo dnf install wazuh-agent
+sudo dnf install wazuh-agent -y
 
 sudo systemctl daemon-reload
 sudo systemctl enable wazuh-agent
 sudo systemctl start wazuh-agent
+check_error "Failed to start Wazuh Agent"
 
 log_message "Setting up CIS profile"
 sudo rm -f "/var/ossec/ruleset/sca/cis_centos*_linux.yml"
